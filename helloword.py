076642333e224c40b30122b1eb077eb3421e50ec
福莱特·比恩 <46574179+flatbean@users.noreply.github.com>
2019-03-20 20:22:42 +0800

    Update helloword.py

--- helloword.py
+++ helloword.py
@@ -13,4 +13,4 @@ if __name__ == '__main__':
     print('Hello, I am first python script on Github')
     print('你好，我是在Github上的第一个python程序!\n')
     print('你好，我的学号后四位是0307。')  
-    
+    print('很好，很好！') 
